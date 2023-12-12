b206250cffba2c3eb9249e53788e10be92f9ab5b
Ch-Praneeth1 <chpraneethvardhanreddy143@gmail.com>
2023-12-12 15:32:59 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
-const demo = require('./demo');
+const http = require('http');
+const fs = require('fs');
+const { stringify } = require('querystring');
 
-const fs = require("node:fs");
 
-// const text = fs.readFileSync('text.txt', 'utf-8')
-// console.log(text)
+const index =fs.readFileSync('index.html','utf-8');
+const data = JSON.parse(fs.readFileSync('dummy.json', 'utf-8'));
+const product = data.products[0];
 
-fs.readFile('text.txt', 'utf-8', (err, text)=>{
-    console.log(text);
+// const index2 = index.JSON()
+// console.log(product)
+
+const server = http.createServer((req,res) => {
+    console.log("server started")
+
+    if(req.url)
+
+    switch(req.url){
+        case '/':
+            res.setHeader("content-type","text/html");
+            res.end(index);
+            break;
+        case '/api':
+            res.setHeader("content-type","application/json");
+            res.end(JSON.stringify(data));
+            break;
+        case '/product':
+            res.setHeader("content-type","text/html");
+            let modifiedIndex = index.replace("**title**",product.title)
+            .replace("**price**",product.price)
+            .replace("**rating**",product.rating)
+            .replace("**url**",product.thumbnail);
+            res.end(modifiedIndex);
+            break;
+        default:
+            break;
+    }
 });
 
-console.log(demo.add(1,2));
+server.listen(8088)
