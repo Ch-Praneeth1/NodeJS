a43641dcaf3c801cb5e394b8f3e3769701296b95
Ch-Praneeth1 <chpraneethvardhanreddy143@gmail.com>
2023-12-12 16:05:38 +0530

    server side rendering using perams

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 const http = require('http');
 const fs = require('fs');
-const { stringify } = require('querystring');
+
 
 
 const index =fs.readFileSync('index.html','utf-8');
 const data = JSON.parse(fs.readFileSync('dummy.json', 'utf-8'));
-const product = data.products[0];
+const products = data.products;
 
 // const index2 = index.JSON()
 // console.log(product)
 
+
 const server = http.createServer((req,res) => {
     console.log("server started")
 
-    if(req.url)
+    // const params = req.url.split('/')
+    // console.log(params)
+
+    if(req.url.startsWith('/product')){
+        const params = req.url.split('/')
+        console.log(params)
+        const productId = params[2]
+        res.setHeader("content-type","text/html");
+        let modifiedIndex = index.replace("**title**",products[productId].title)
+        .replace("**price**",products[productId].price)
+        .replace("**rating**",products[productId].rating)
+        .replace("**url**",products[productId].thumbnail);
+        res.end(modifiedIndex);
+    }
 
     switch(req.url){
         case '/':
@@ -23,14 +37,6 @@ const server = http.createServer((req,res) => {
         case '/api':
             res.setHeader("content-type","application/json");
             res.end(JSON.stringify(data));
-            break;
-        case '/product':
-            res.setHeader("content-type","text/html");
-            let modifiedIndex = index.replace("**title**",product.title)
-            .replace("**price**",product.price)
-            .replace("**rating**",product.rating)
-            .replace("**url**",product.thumbnail);
-            res.end(modifiedIndex);
             break;
         default:
             break;
