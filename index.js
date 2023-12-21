89881029b0f13fbd7755ffd72c8f23b81947ccee
Ch-Praneeth1 <chpraneethvardhanreddy143@gmail.com>
2023-12-21 10:06:35 +0530

    server creation using express

--- index.js
+++ index.js
@@ -1,47 +1,108 @@
-const http = require('http');
+// const http = require('http');
 const fs = require('fs');
 
 
 
-const index =fs.readFileSync('index.html','utf-8');
+// const index =fs.readFileSync('index.html','utf-8');
 const data = JSON.parse(fs.readFileSync('dummy.json', 'utf-8'));
 const products = data.products;
 
-// const index2 = index.JSON()
-// console.log(product)
+
+// // const index2 = index.JSON()
+// // console.log(product)
 
 
-const server = http.createServer((req,res) => {
-    console.log("server started")
+// const server = http.createServer((req,res) => {
+//     console.log("server started")
 
-    // const params = req.url.split('/')
-    // console.log(params)
+//     // const params = req.url.split('/')
+//     // console.log(params)
 
-    if(req.url.startsWith('/product')){
-        const params = req.url.split('/')
-        console.log(params)
-        const productId = params[2]
-        res.setHeader("content-type","text/html");
-        let modifiedIndex = index.replace("**title**",products[productId].title)
-        .replace("**price**",products[productId].price)
-        .replace("**rating**",products[productId].rating)
-        .replace("**url**",products[productId].thumbnail);
-        res.end(modifiedIndex);
-        return;
+//     if(req.url.startsWith('/product')){
+//         const params = req.url.split('/')
+//         console.log(params)
+//         const productId = params[2]
+//         res.setHeader("content-type","text/html");
+//         let modifiedIndex = index.replace("**title**",products[productId].title)
+//         .replace("**price**",products[productId].price)
+//         .replace("**rating**",products[productId].rating)
+//         .replace("**url**",products[productId].thumbnail);
+//         res.end(modifiedIndex);
+//         return;
+//     }
+
+//     switch(req.url){
+//         case '/':
+//             res.setHeader("content-type","text/html");
+//             res.end(index);
+//             break;
+//         case '/api':
+//             res.setHeader("content-type","application/json");
+//             res.end(JSON.stringify(data));
+//             break;
+//         default:
+//             break;
+//     }
+// });
+
+// server.listen(8088)
+
+
+
+
+
+
+const express = require('express');
+
+
+//bodyParser
+const server = express();
+
+server.use(express.json());
+//universal middleware
+// server.use('/',(req,res,next) => {
+//     console.log('universal middleware')
+//     console.log(req.method, req.ip ,req.hostname, new Date() , req.get('user-agent'))   // Universal middleware can be used for whole application such as user login can be done
+//     next()                                          // trought middleware authorized users can be alloweed and others cannot   
+// })
+
+
+
+//rout middleware       specified for some perticular routs we need
+const auth = (req,res,next) => {
+    console.log("authentication middleware")
+    if(req.body.password=='123'){
+        console.log("user is authorized")
+        next()
+    }else{
+        console.log("user is not authorized")
+        res.sendStatus(401);
     }
+}
 
-    switch(req.url){
-        case '/':
-            res.setHeader("content-type","text/html");
-            res.end(index);
-            break;
-        case '/api':
-            res.setHeader("content-type","application/json");
-            res.end(JSON.stringify(data));
-            break;
-        default:
-            break;
-    }
-});
+server.get('/',auth,(req,res)=>{
+    res.send("<h1>hello world get</h1>")
+    // res.sendFile('C:/Users/chpra/Desktop/NodeApp/index.html')
+    // res.json(products)
+})
 
-server.listen(8088)
+
+server.post('/',(req,res)=>{
+    res.send("<h1>hello world post</h1>")
+})
+
+server.delete('/',(req,res)=>{
+    res.send("<h1>hello world delete</h1>")
+})
+
+server.patch('/',(req,res)=>{
+    res.send("<h1>hello world patch</h1>")
+})
+
+server.put('/',(req,res)=>{
+    res.send("<h1>hello world put</h1>")
+})
+
+server.listen(8088,() => {
+    console.log('server started')
+})
